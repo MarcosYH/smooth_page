828e0592aa058500c2bf28a9726eca5a86fc7400
MarcosYH <marcosdeimos18@gmail.com>
2023-01-18 10:25:56 +0100

    Img logo

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,7 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:smoth_page/page1.dart';
 import 'package:smoth_page/page2.dart';
 import 'package:smoth_page/page3.dart';
+import 'package:adobe_xd/adobe_xd.dart';
 
 class homePage extends StatelessWidget {
  // const homePage({Key? key}) : super(key: key);
@@ -11,36 +12,74 @@ class homePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+      backgroundColor: Colors.white,
+
+      body: Stack(
         children: [
-          SizedBox(
-            height: 400,
+          Container(
             child: PageView(
               controller: _controller ,
               children: [
                 page1(),
                 page2(),
                 page3(),
-
               ],
             ),
           ),
-          SmoothPageIndicator(
-              controller: _controller,
-              count: 3,
-           effect: ExpandingDotsEffect(
-              activeDotColor: Colors.deepOrange,
-             dotColor: Colors.grey.withOpacity(1),
-             dotHeight: 10,
-             dotWidth: 20,
+          Align(
+            alignment: Alignment(-0.077, 0.57),
+            child:  SmoothPageIndicator(
+              controller: _controller, count: 3,
+              effect: ExpandingDotsEffect(
+                activeDotColor: Color(0xff6b7ffd),
+                dotColor: Colors.grey.withOpacity(1),
+                dotHeight: 10,
+                dotWidth: 20,
+
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment(-0.077, 0.9),
+            child: SizedBox(
+              width: 232.0,
+              height: 45.0,
+              child: Stack(
+                children: <Widget>[
+                  Center(
+                    child: ElevatedButton(
+                      child: Text(
+                        'Commencer',),
+                      style: ElevatedButton.styleFrom (
+                        backgroundColor: const Color(0xff2195f2),
+                        textStyle: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w700,
+                        ),
+                        minimumSize: Size(232.0, 45.0),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                      ),
+                      onPressed: () {
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),// boutton connexion avec couleur gris fonçé
+
+
+
+        ],
+
+
 
            ),
+          
 
-          ),
-          ElevatedButton(onPressed: (){}, child: Text('Connexion',))
-        ],
-      ),
 
     );
   }
--- lib/page1.dart
+++ lib/page1.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:adobe_xd/adobe_xd.dart';
 class page1 extends StatelessWidget {
   const page1({Key? key}) : super(key: key);
 
@@ -7,12 +7,74 @@ class page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Padding(
-        padding: const EdgeInsets.all(40),
+      body: Container(
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: Container(
-            child: Image.asset("assets/images/ui0.png",),
+          child:Stack(
+            children: <Widget> [
+              Pinned.fromPins(
+                Pin(start: 0.0, end: 0.0),
+                Pin(size: 406.0, start: 0.0),
+                child: Container(
+
+                  decoration: BoxDecoration(
+                    color: const Color(0xff6c80fe),
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color(0x1f000000),
+                        offset: Offset(0, 15),
+                        blurRadius: 30,
+                      ),
+                    ],
+                  ),
+                ),
+              ),// Couleur bleu en fond
+
+              Pinned.fromPins(
+                Pin(size: 310.0, start: -115.0),
+                Pin(size: 310.0, start: -173.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xff61e0dd),
+                    borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                  ),
+                ),
+              ),//positionnement de la couleur en haut à gauche
+
+              Pinned.fromPins(
+                Pin(size: 310.0, end: -144.0),
+                Pin(size: 310.0, middle: 0.3586),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xffbaf85f),
+                    borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                  ),
+                ),
+              ),//Positionnement de la couleur vert façon là
+              Pinned.fromPins(
+                Pin(size: 177.0, middle: 0.5),
+                Pin(size: 133.0, start: 50),
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: const AssetImage("assets/images/logo2.png"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+
+              Pinned.fromPins(
+                Pin(start: -74.8, end: -74.8),
+                Pin(size: 450, middle: 0.5),
+                child: Image.asset("assets/images/voiture_white.png",),
+              ), // Positionnement de l'image de la voiture
+
+            ],
+
             //color: Colors.blue,
           ),
         ),
